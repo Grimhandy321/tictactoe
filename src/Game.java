65997bb0ed2c2cf7107002f430eb83a5e64db148
prihoda <prihoda@spsejecna.cz>
2024-03-18 10:11:13 +0100

    grid checks

--- src/Game.java
+++ src/Game.java
@@ -18,7 +18,39 @@ public class Game {
         return gameGrid[x][y];
     }
     public String checkWinState(){
-
+        for(int x = 0; x< gameGrid.length;x++ ){
+            if(!Objects.equals(gameGrid[x][0], "")){
+                if(Objects.equals(gameGrid[x][0], tokenA) && Objects.equals(gameGrid[x][1], tokenA) && Objects.equals(gameGrid[x][2], tokenA)){
+                    return tokenA;
+                }
+                if(Objects.equals(gameGrid[x][0], tokenB) && Objects.equals(gameGrid[x][1], tokenB) && Objects.equals(gameGrid[x][2], tokenB)){
+                    return tokenA;
+                }
+            }
+        }
+        for(int y = 0; y< gameGrid.length;y++ ){
+            if(!Objects.equals(gameGrid[0][y], "")){
+                if(Objects.equals(gameGrid[0][y], tokenA) && Objects.equals(gameGrid[1][y], tokenA) && Objects.equals(gameGrid[2][y], tokenA)){
+                    return tokenA;
+                }
+                if(Objects.equals(gameGrid[0][y], tokenB) && Objects.equals(gameGrid[1][y], tokenB) && Objects.equals(gameGrid[2][y], tokenB)){
+                    return tokenA;
+                }
+            }
+        }
+        if(Objects.equals(gameGrid[0][0], tokenB) && Objects.equals(gameGrid[1][1], tokenB) && Objects.equals(gameGrid[2][0], tokenB)){
+            return tokenB;
+        }
+        if(Objects.equals(gameGrid[0][0], tokenA) && Objects.equals(gameGrid[1][1], tokenA) && Objects.equals(gameGrid[2][0], tokenA)){
+            return tokenA;
+        }
+        if(Objects.equals(gameGrid[2][2], tokenB) && Objects.equals(gameGrid[1][1], tokenB) && Objects.equals(gameGrid[0][2], tokenB)){
+            return tokenB;
+        }
+        if(Objects.equals(gameGrid[2][2], tokenA) && Objects.equals(gameGrid[1][1], tokenA) && Objects.equals(gameGrid[0][2], tokenA)){
+            return tokenA;
+        }
+        return "";
     }
 
     public void AddToken(int x ,int y,String token){
