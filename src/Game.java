6d5a443d128db3e07656ebaec30da30ad8eb773c
prihoda <prihoda@spsejecna.cz>
2024-03-18 09:52:42 +0100

    game

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,29 @@
+import java.util.Objects;
+
 public class Game {
     public String[][] gameGrid = new String[3][3];
+    public String tokenA ;
+    public String tokenB;
+
+    public Game(String tokenA, String tokenB) {
+        this.tokenA = tokenA;
+        this.tokenB = tokenB;
+    }
+
     public String DrawGame(){
         String ret = new String();
         return ret;
     }
-    public void AddToken(int x ,int y){
+    public String getToken(int x,int y){
+        return gameGrid[x][y];
+    }
+    public String checkWinState(){
 
+    }
+
+    public void AddToken(int x ,int y,String token){
+        if(Objects.equals(gameGrid[x][y], "")){
+            gameGrid[x][y]= token;
+        }
     }
 }
